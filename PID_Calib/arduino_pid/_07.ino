adf376a34a7afa88eede224b8d932d340634fc93
Aakarsh <aakarsh.s.saini@gmail.com>
2018-09-09 03:18:13 +0530

    updated loop

--- PID_Calib/arduino_pid/_07.ino
+++ PID_Calib/arduino_pid/_07.ino
@@ -1,12 +1,16 @@
 void loop() {
   /////
   if (test == true) {
+    lpf(60);//input_speed for low pass filter
     ch = 'f';
     move(ch);
   }
   if (test == false) {
     ch = 's';
     move(ch);
+    prev_output = 0.0;
+    m[0].pwm = 0;
+    m[2].pwm = 0;
   }
   pid_yaw(360.0);
 }
