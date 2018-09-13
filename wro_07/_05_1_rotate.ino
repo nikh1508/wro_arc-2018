e50787434eb86deeb84252ec8c62b0912e51ab6e
nikh1508 <nikh150897@gmail.com>
2018-09-13 22:51:43 +0530

    improved rotation and added timeout feature

--- wro_07/_05_1_rotate.ino
+++ wro_07/_05_1_rotate.ino
@@ -1,4 +1,5 @@
 double rotationSpeed, initialSpeed;
+bool timeOut = false;
 double diff(float x, float y) {
   double tmp = abs(x - y);
   double diff = min(tmp, abs(360 - tmp));
@@ -16,9 +17,15 @@ double abs1(double x) {
   else return x;
 }
 
-void rotateTo(double angle, int dir, int ROT_SPEED = 100) {
+void rotateTo(double angle, int dir, int ROT_SPEED = 130) {
+  timeOut = false;
+  reset_encoder();
   initialSpeed = rotationSpeed = ROT_SPEED;
   angle = abs1(angle);
+  bno();
+  double initialDiff = abs1(diff(angle, yaw));
+  double divisor =  initialDiff / 90.0;
+  double currentDiff = initialDiff;
   switch (dir) {
     case CW:
       digitalWrite(m[0].a, LOW);
@@ -37,26 +44,34 @@ void rotateTo(double angle, int dir, int ROT_SPEED = 100) {
       digitalWrite(m[2].b, HIGH);
       break;
   }
-//  delay(50);
-//  encoder_data0();
-//  encoder_data1();
-  bno();
-  //  Serial.println("here");
   long long last_time = millis();
-  while (abs1(diff(angle, yaw)) > 1.0) {
-//    Serial.println(yaw);
+  encoder_data0();
+  encoder_data1();
+  int lastEncCtr0 = encoder_0, lastEncCtr1 = encoder_1;
+  while (currentDiff > 1.0 && !timeOut) {
+    //    Serial.println(yaw);
     bno();
-//    encoder_data0();
-//    encoder_data1();
+    currentDiff = abs1(diff(angle, yaw));
+    if (currentDiff < (initialDiff / 2.0) && millis() - last_time > 50) {
+      last_time = millis();
+      encoder_data0();
+      encoder_data1();
+      if (lastEncCtr0 == encoder_0 && lastEncCtr1 == encoder_1) {
+        Serial.println("Rotation Timeout");
+        timeOut = true;
+      }
+      lastEncCtr0 = encoder_0;
+      lastEncCtr1 = encoder_1;
+    }
     analogWrite(m[0].pwm_pin, rotationSpeed);
     analogWrite(m[1].pwm_pin, rotationSpeed);
     analogWrite(m[2].pwm_pin, rotationSpeed);
-    rotationSpeed = sin( abs1(diff(angle, yaw)) / 180.0 * 3.14) * initialSpeed;
+    rotationSpeed = sin( (abs1(diff(angle, yaw)) / divisor) / 180.0 * 3.14) * initialSpeed;
     //    delay(2);
   }
 
   stop();
-  ch='s';
+  ch = 's';
   //  rotate_desired = angle;
   bno();
   //  Serial.println("Current YAW::"+String(yaw));
