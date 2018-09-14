b3af971dc88daa2769b92a705bc36012d51a25f2
Aakarsh <aakarsh.s.saini@gmail.com>
2018-09-14 09:12:09 +0530

    added extra functions

--- wro_07/_10_extra_functions.ino
+++ wro_07/_10_extra_functions.ino
@@ -21,7 +21,7 @@ void dump_error() {
   while (ch != 's') {
     false_pid(270.0, 0);
     encoder_data0();
-    encoder_0=abs(encoder_0);
+    encoder_0 = abs(encoder_0);
     if (encoder_0 >= 400) {
       stop();
       ch = 's';
@@ -76,5 +76,50 @@ void dump_error() {
     }
     else
       dline2 = 0;
+  }
+}
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+void goto_stacking(int enc0, int enc1, int dir) {
+  reset_feedback();
+  ///////////----->>>forward
+  digitalWrite(m[0].a, HIGH);
+  digitalWrite(m[0].b, LOW);
+  //1///
+  digitalWrite(m[1].a, HIGH);
+  digitalWrite(m[1].b, HIGH);
+  //2
+  digitalWrite(m[2].a, HIGH);
+  digitalWrite(m[2].b, LOW);
+  ////
+  m[0].pwm = m[2].pwm = speed[slow];
+  analogWrite(m[0].pwm_pin, m[0].pwm);
+  analogWrite(m[1].pwm_pin, 0);
+  analogWrite(m[2].pwm_pin, m[0].pwm);
+  /////
+  myPID.SetTunings(kp[slow], ki[slow], kd[slow]);
+  while (ch != 's') {
+    false_pid(270.0, 1);
+    encoder_data0();
+    encoder_0 = abs(encoder_0);
+    if (encoder_0 >= enc0) {
+      stop();
+      ch = 's';
+    }
+  }
+  reset_feedback();
+  if (dir > 0)
+    right_value();
+  else
+    left_value();
+  /////////////////
+  while (ch != 's') {
+    false_pid_sideways(270.0, dir);
+    encoder_data1();
+    encoder_1 = abs(encoder_1);
+    if (encoder_1 >= enc1) {
+      stop();
+      ch = 's';
+    }
   }
 }
