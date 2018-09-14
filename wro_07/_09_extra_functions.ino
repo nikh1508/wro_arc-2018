8c8389a3203dbb4b2882ebcdd14d34110737f6f9
Aakarsh <aakarsh.s.saini@gmail.com>
2018-09-14 21:41:13 +0530

    added dump error extension

--- wro_07/_09_extra_functions.ino
+++ wro_07/_09_extra_functions.ino
@@ -1,54 +1,56 @@
-void dump_error(int enc0=400,int enc1=400,double angle = 270.0) {
+void dump_error(int enc0 = 400, int enc1 = 400, double angle = 270.0, int area = 0) {
   reset_feedback();
-  /////---->backward till encoder
-  digitalWrite(m[0].a, LOW);
-  digitalWrite(m[0].b, HIGH);
-  //1
-  digitalWrite(m[1].a, HIGH);
-  digitalWrite(m[1].b, HIGH);
-  //2
-  digitalWrite(m[2].a, LOW);
-  digitalWrite(m[2].b, HIGH);
-  /////
-  /////
-  m[0].pwm = m[2].pwm = speed[slow];
-  analogWrite(m[0].pwm_pin, m[0].pwm);
-  analogWrite(m[1].pwm_pin, 0);
-  analogWrite(m[2].pwm_pin, m[0].pwm);
-  //
-  myPID.SetTunings(kp[slow], ki[slow], kd[slow]);
-  /////
-  while (ch != 's') {
-    false_pid(angle, 0);
-    encoder_data0();
-    encoder_0 = abs(encoder_0);
-    if (encoder_0 >= enc0) {
-      stop();
-      ch = 's';
+  if (area == 0) {
+    /////---->backward till encoder
+    digitalWrite(m[0].a, LOW);
+    digitalWrite(m[0].b, HIGH);
+    //1
+    digitalWrite(m[1].a, HIGH);
+    digitalWrite(m[1].b, HIGH);
+    //2
+    digitalWrite(m[2].a, LOW);
+    digitalWrite(m[2].b, HIGH);
+    /////
+    /////
+    m[0].pwm = m[2].pwm = speed[slow];
+    analogWrite(m[0].pwm_pin, m[0].pwm);
+    analogWrite(m[1].pwm_pin, 0);
+    analogWrite(m[2].pwm_pin, m[0].pwm);
+    //
+    myPID.SetTunings(kp[slow], ki[slow], kd[slow]);
+    /////
+    while (ch != 's') {
+      false_pid(angle, 0);
+      encoder_data0();
+      encoder_0 = abs(encoder_0);
+      if (encoder_0 >= enc0) {
+        stop();
+        ch = 's';
+      }
     }
+    ch = '\0';
+    /////////----->>>>forward
+    digitalWrite(m[0].a, HIGH);
+    digitalWrite(m[0].b, LOW);
+    //1///
+    digitalWrite(m[1].a, HIGH);
+    digitalWrite(m[1].b, HIGH);
+    //2
+    digitalWrite(m[2].a, HIGH);
+    digitalWrite(m[2].b, LOW);
+    ////
+    m[0].pwm = m[2].pwm = speed[slow];
+    analogWrite(m[0].pwm_pin, m[0].pwm);
+    analogWrite(m[1].pwm_pin, 0);
+    analogWrite(m[2].pwm_pin, m[0].pwm);
+    //
+    myPID.SetTunings(kp[slow], ki[slow], kd[slow]);
+    while (ch != 's') {
+      false_pid(angle, 1);
+      line_stop();
+    }
+    ch = '\0';
   }
-  ch = '\0';
-  /////////----->>>>forward
-  digitalWrite(m[0].a, HIGH);
-  digitalWrite(m[0].b, LOW);
-  //1///
-  digitalWrite(m[1].a, HIGH);
-  digitalWrite(m[1].b, HIGH);
-  //2
-  digitalWrite(m[2].a, HIGH);
-  digitalWrite(m[2].b, LOW);
-  ////
-  m[0].pwm = m[2].pwm = speed[slow];
-  analogWrite(m[0].pwm_pin, m[0].pwm);
-  analogWrite(m[1].pwm_pin, 0);
-  analogWrite(m[2].pwm_pin, m[0].pwm);
-  //
-  myPID.SetTunings(kp[slow], ki[slow], kd[slow]);
-  while (ch != 's') {
-    false_pid(angle, 1);
-    line_stop();
-  }
-  ch = '\0';
   reset_feedback();
   /////////////////////////////////////////////////////
   left_value();
