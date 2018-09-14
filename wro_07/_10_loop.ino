402230f3541a5f44407f2acbfc596eb246adf52f
Aakarsh <aakarsh.s.saini@gmail.com>
2018-09-14 21:41:38 +0530

    added dump error extensions

--- wro_07/_10_loop.ino
+++ wro_07/_10_loop.ino
@@ -3,7 +3,7 @@ void loop() {
   //  servo_camera();
   /////
   //  goto_stacking(500, 500, 1, 0.0);////(int enc0, int enc1, int dir, double angle = 270.0) {/////////dir 1-->right.....dir 2-->left
-  //  dump_error(400,400,0.0);//////with default arguments
+  //  dump_error(400,400,0.0,1);//////with default arguments
   //  while (1);
 
 }
