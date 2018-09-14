7450f937c2d1f5bf2a59ea1751be7af324ae085d
Aakarsh <aakarsh.s.saini@gmail.com>
2018-09-14 10:05:36 +0530

    added description to functions

--- wro_07/_10_loop.ino
+++ wro_07/_10_loop.ino
@@ -2,7 +2,7 @@ void loop() {
   //  sequnece();
   //  servo_camera();
   /////
-  //  goto_stacking(500, 500, 1, 0.0);//////with default arguments
+  //  goto_stacking(500, 500, 1, 0.0);////(int enc0, int enc1, int dir, double angle = 270.0) {/////////dir 1-->right.....dir 2-->left
   //  dump_error(400,400,0.0);//////with default arguments
   //  while (1);
 
